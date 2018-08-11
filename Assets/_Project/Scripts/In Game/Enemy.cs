ecd3db989a7df986d890c311bea9461c34c17785
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-08-11 20:26:25 -0300

    [UPDATE] Tubarao comecao a ficar em volta do player

--- Assets/_Project/Scripts/In Game/Enemy.cs
+++ Assets/_Project/Scripts/In Game/Enemy.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public abstract class Enemy : MonoBehaviour {
 
-	
+	protected float speed;
+    protected Rigidbody2D rb2D;
+    protected Vector2 velocity;
+
+    protected virtual void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
 
 }
---
+++ Assets/_Project/Scripts/In Game/Tubarao.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tubarao : Enemy {
+
+	public Transform urso;
+	public float distanciaMinima;
+	private bool alcancouUrso = false;
+
+	private Sprite plataformaUrso;
+	private Bounds plataformaBounds;
+
+	private Animator animator;
+
+	protected override void Awake()
+	{
+		speed = 2f;
+		animator = GetComponent<Animator>();
+
+		animator.enabled = false;
+		// Nothing so far
+		base.Awake();
+	}
+
+	void Start()
+	{
+		// plataformaUrso = GameObject.Find("Plataforma").GetComponent<SpriteRenderer>().sprite;
+		// plataformaBounds = plataformaUrso.bounds;
+	}
+
+	void Update()
+	{
+		if(!alcancouUrso)
+		{
+			Vector2 direcao = (urso.position - transform.position).normalized;
+			velocity = direcao * speed;
+
+			// Se alcancou urso
+			if(UrsoPerto())
+			{
+				alcancouUrso = true;
+
+				// ...seta o tubarao como filho do urso
+				transform.SetParent(urso);
+
+
+				animator.enabled = true;
+				animator.SetTrigger("isSurrounding");
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb2D.velocity = velocity;
+	}
+
+	private bool UrsoPerto()
+	{
+		return Vector2.Distance(urso.position, transform.position) <= distanciaMinima;
+	}
+}
