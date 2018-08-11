0412491b30c914bffd09c99c2035c741f5095809
Gustavo Silva <xgustavossilva@gmail.com>
2018-08-11 18:28:18 -0300

    [Add] Added player movement

---
+++ Assets/_Project/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : Singleton<PlayerManager> {
+
+	public GameObject player;
+	public PlayerMotor playerMotor;
+	public PlayerController playerController;
+
+	protected override void Awake()
+	{
+		IsPersistentBetweenScenes = false;
+		base.Awake();
+	}
+
+	private void Start() 
+	{
+		playerMotor = player.GetComponent<PlayerMotor>();
+		playerController = player.GetComponent<PlayerController>();
+	}
+
+}
---
+++ Assets/_Project/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager> {
+
+	public Slider staminaSlider;
+
+	protected override void Awake()
+	{
+		IsPersistentBetweenScenes = false;
+		base.Awake();
+	}
+
+	private void Start() 
+	{
+		PlayerManager.Instance.playerMotor.ChangeStamina += ChangeUIStamina;
+	}
+
+
+	void ChangeUIStamina(float qtd)
+	{
+		staminaSlider.value = qtd;
+	}
+}
