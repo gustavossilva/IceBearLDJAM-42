1e07fa3210fe9413ac549bdcec1695ed662b94a7
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-08-12 00:38:46 -0300

    [Add] IceSystem and ice behaviours (Early)

---
+++ Assets/IceBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class IceBehaviour : MonoBehaviour {
+
+	IceController controlRef;
+
+	[SerializeField] private int life = 100;
+
+	public int Life {
+		private set{}
+		get{
+			return this.life;
+		}
+	}
+
+	[SerializeField] IcePosition myPosition;
+
+	public Action DamageTaken;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake () {
+		controlRef = IceController.Instance;
+	}
+
+	private void Start () {
+		StartCoroutine (SunBurn ());
+	}
+
+	public void TakeDamage (int damage) {
+		this.life = Mathf.Clamp (this.life + damage, 0, 100);
+		if(DamageTaken != null){
+			DamageTaken.Invoke();
+		}
+	}
+
+	private void OnTriggerEnter2D (Collider2D other) {
+		if (controlRef.canHit) {
+
+		}
+	}
+
+	public bool isAlive(){
+		return this.life > 0? true : false;
+	}
+
+	private IEnumerator SunBurn () {
+		while (this.life > 0) {
+			yield return new WaitForSeconds (1);
+			if (controlRef.coldWater) {
+				TakeDamage(controlRef.coldWaterHeal);
+			} else {
+				TakeDamage(-controlRef.sunDamage);
+			}
+		}
+		//Tira da lista antes de se desativar
+		controlRef.iceScripts[(int)myPosition] = null;
+		//Dispara couritina de animação de então desativa
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/LifeSliderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LifeSliderController : MonoBehaviour {
+
+	[SerializeField] GameObject iceController;
+	[SerializeField] public IceBehaviour[] iceScripts;
+	[SerializeField] private Slider iceLifeSlider;
+	int totalLife; 
+	// Use this for initialization
+	void Start () {
+		iceScripts = iceController.GetComponentsInChildren<IceBehaviour> ();
+		for (int i = 0; i < iceScripts.Length; i++) {
+			iceScripts[i].DamageTaken += updateSliderValue;
+		}
+	}
+
+	void updateSliderValue () {
+		totalLife = 0;
+		for(int i =0; i< iceScripts.Length;i++){
+			totalLife += iceScripts[i].Life;
+		}
+		iceLifeSlider.value = totalLife/5;
+	}
+
+}
