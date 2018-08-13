cec43051a0cd9788478540b4c4c3f6d61f163fda
Gustavo Silva <xgustavossilva@gmail.com>
2018-08-13 01:51:10 -0300

    [Add] new track system

--- Assets/_Project/Scripts/In Game/UIManager.cs
+++ Assets/_Project/Scripts/In Game/UIManager.cs
@@ -7,8 +7,29 @@ public class UIManager : Singleton<UIManager> {
 
 	public Slider staminaSlider;
 
+	//Background to track
+	public Transform scrollingBackground;
+
+	//Track Images
+	public Image bearTrackImage;
+
+	//Track positions
+	public RectTransform[] trackPositions = new RectTransform[6];
+	public RectTransform startPosition;
+	public RectTransform endPosition;
+	/*public RectTransform position2;
+	public RectTransform position3;
+	public RectTransform position4;
+	public RectTransform position5;
+	*/
+
+	public float newTrackPos;
+	public float distance;
+	Vector2 newPos = Vector2.zero;
+
 	protected override void Awake()
 	{
+		distance = -0.7f-173.2f;
 		IsPersistentBetweenScenes = false;
 		base.Awake();
 	}
@@ -16,6 +37,15 @@ public class UIManager : Singleton<UIManager> {
 	private void Start() 
 	{
 		PlayerManager.Instance.playerMotor.ChangeStamina += ChangeUIStamina;
+	}
+	private void Update() {
+		//-430 - 0
+		//420 - 2000
+		//850 = pos inicial + pos final (distancia)
+		newTrackPos = (((scrollingBackground.position.x * (Mathf.Abs(startPosition.anchoredPosition.x) + Mathf.Abs(endPosition.anchoredPosition.x)))/distance));
+		newPos.Set(startPosition.anchoredPosition.x + newTrackPos,bearTrackImage.rectTransform.anchoredPosition.y);
+		//430 = posinicial
+		bearTrackImage.rectTransform.anchoredPosition = newPos;
 	}
 
 
